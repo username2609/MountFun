6340dcc8faa565ccd4260fd2d0b7d84c2a9b1134
FelixT9 <131136492+FelixT9@users.noreply.github.com>
2023-06-13 08:03:50 +0200

    Add files via upload

---
+++ MountFun 13.06.23  08_02/Hindernis_view.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+/**
+ * Beschreiben Sie hier die Klasse Hindernis_model.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Hindernis_view
+{
+    protected int x, y,höhe,breite;
+    public Hindernis_view(){
+
+    }
+    public void setY(int yNeu){
+        y=yNeu;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int getBreite(){
+        return breite;
+    }
+
+    public int getHöhe(){
+        return höhe;
+    }
+
+    
+
+    public void ortSetzen(int yNeu){
+
+    }
+
+    public Hindernis_model getModel(){
+        return null;
+    }
+}
+
---
+++ MountFun 13.06.23  08_02/SpielStart.java
@@ -0,0 +1,15 @@
+
+/**
+ * Beschreiben Sie hier die Klasse SpielStart.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class SpielStart
+{
+    public static void main(String[] args) {
+       Controller controller= new Controller();
+       controller.Main();
+       controller.startTakt();
+    }
+}
