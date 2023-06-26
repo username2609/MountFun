fdf8956c04de43ddd3d51762129d0ee5ac49f1cd
lucaruff05 <131137298+lucaruff05@users.noreply.github.com>
2023-06-26 20:57:56 +0200

    Add files via upload

---
+++ MountFun 26.06.23 20_53/Baum_view.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Baum_view extends Hindernis_view
+{
+    private JLabel l1;
+
+    public Baum_view(JLayeredPane panel, String Dateipfad,int x, int y) {
+        ImageIcon icon = new ImageIcon(Dateipfad + "Baum.png");
+        Image Bild = icon.getImage().getScaledInstance(209, 242, Image.SCALE_SMOOTH);
+        ImageIcon skaliertesBild = new ImageIcon(Bild);
+        l1 = new JLabel(skaliertesBild);
+        l1.setBounds(x-150, y, icon.getIconWidth(), icon.getIconHeight());
+
+        panel.add(l1,JLayeredPane.MODAL_LAYER);
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    @Override public JLabel getL1(){
+        return l1;
+    }
+
+    @Override public void ortSetzen(int yNeu){
+        l1.setLocation(l1.getX(), yNeu);
+    }
+
+    @Override public void baumNachObenRücken(){
+        System.out.println("Hi");
+        l1.setLocation(l1.getX(),l1.getY()-1);
+    }
+}
