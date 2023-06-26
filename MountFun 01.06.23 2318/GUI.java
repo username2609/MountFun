2d7043b429eab1b8e59b6ebf6d18212eb4bfbda0
username2609 <130965212+username2609@users.noreply.github.com>
2023-06-26 09:18:15 +0200

    Update GUI.java

--- MountFun 01.06.23 2318/GUI.java
+++ MountFun 01.06.23 2318/GUI.java
@@ -1,8 +1,8 @@
 package GUIMFun;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class GUI
 {
@@ -10,7 +10,6 @@ public class GUI
     private JFrame fenster;
     private JDialog dialog;
     private JLabel highscore;
-    private JLabel münzen;
     private JButton anpassung;
     private JButton spielen;
     private JButton hintergrund;
@@ -22,21 +21,13 @@ public class GUI
         fenster.setSize(2000,1200);
         fenster.setVisible(false);
         fenster.setLayout(null);
+        fenster.detEnabled(false);
         
         dialog = new JDialog();
         dialog.setTitle("MountFun");
         dialog.setSize(1920, 1040);
         dialog.setVisible(true);
-        
-        highscore = new JLabel ("Highscore: ");
-        highscore.setBounds(90, 10, 200, 100);
-        highscore.setVisible(true);
-        highscore.setForeground(Color.black);
-        
-        münzen = new JLabel ("Münzen: ");
-        münzen.setBounds(1750, 10, 200, 100);
-        münzen.setVisible(true);
-        münzen.setForeground(Color.black);
+        dialog.setEnabled(false);
         
         anpassung = new JButton ("Anpassung");
         anpassung.setLocation(892,590);
@@ -45,14 +36,11 @@ public class GUI
         anpassung.setEnabled(true);
         anpassung.setBackground(Color.blue);
         anpassung.setForeground(Color.black);
-        
-        spielen = new JButton ("Spielen");
-        spielen.setLocation(800, 520);
-        spielen.setSize(300, 40);
-        spielen.setVisible(true);
-        spielen.setEnabled(true);
-        spielen.setBackground(Color.blue);
-        spielen.setForeground(Color.black);
+
+        highscore = new JLabel ("Highscore: ");
+        highscore.setBounds(90, 10, 200, 100);
+        highscore.setVisible(true);
+        highscore.setForeground(Color.black);
         
         hintergrund = new JButton ("");
         hintergrund.setSize(1920, 1040);
@@ -61,20 +49,26 @@ public class GUI
         hintergrund.setBackground(Color.blue);
         hintergrund.setLayout(new BorderLayout());
         JLabel Hintergrund = new JLabel (new ImageIcon("C:\\Users\49151\\OneDrive\\Bilder\\Hintergrund (1).png"));
-        hintergrund.setLayout(new FlowLayout());    
-        
+        hintergrund.setLayout(new FlowLayout()); 
+
+        spielen = new JButton ("Spielen");
+        spielen.setLocation(800, 520);
+        spielen.setSize(300, 40);
+        spielen.setVisible(true);
+        spielen.setEnabled(true);
+        spielen.setBackground(Color.blue);
+        spielen.setForeground(Color.black);
+
+        dialog.add(highscore);
+        dialog.add(anpassung);
+        dialog.add(hintergrund);
+        dialog.add(spielen);
         
         fenster.add(highscore);
-        fenster.add(münzen);
         fenster.add(anpassung);
-        fenster.add(spielen);
         fenster.add(hintergrund);
+        fenster.add(spielen);
         
-        dialog.add(highscore);
-        dialog.add(münzen);
-        dialog.add(anpassung);
-        dialog.add(spielen);
-        dialog.add(hintergrund);
 }
 
 } 
