6b3fc01013a5b36d7f1953318bd43f0d665527c0
FelixT9 <131136492+FelixT9@users.noreply.github.com>
2023-06-26 18:28:15 +0200

    Add files via upload

---
+++ MountFun 23.06.23 22_50 Luca_fixed/Hindernisbeobachter.java
@@ -0,0 +1,5 @@
+
+public interface Hindernisbeobachter
+{
+     public void HindernisseGeaendert();
+}
---
+++ MountFun 23.06.23 22_50 Luca_fixed/SpielStart.java
@@ -0,0 +1,8 @@
+
+public class SpielStart
+{
+    public static void main(String[] args) {
+       Controller controller = new Controller();
+       controller.startTakt();
+    }
+}
