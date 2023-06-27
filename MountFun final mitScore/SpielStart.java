d807afb9e5cc779d66fc8473439153f8e6579280
FelixT9 <131136492+FelixT9@users.noreply.github.com>
2023-06-27 23:29:12 +0200

    Add files via upload

---
+++ MountFun final mitScore/SpielStart.java
@@ -0,0 +1,8 @@
+
+public class SpielStart
+{
+    public static void main(String[] args) {
+       Controller controller = new Controller(0);
+       System.out.println("Zum Start Pfeiltaste drücken!");
+    }
+}
